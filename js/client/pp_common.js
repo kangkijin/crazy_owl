b145c325de0b746dcc3741fe55f42de927bd364a
Kangkijin <rkdrlwls468@gmail.com>
2024-06-17 19:53:59 +0900

    all files upload

---
+++ js/client/pp_common.js
@@ -0,0 +1,363 @@
+/* 클라이언트 ui 스크립트 */
+/*var sessionUserId = '';
+
+if(sesseionUserId == ""){
+   //로그인 안했을때
+	console.log("로그인x");
+} else {
+   //로그인 했을때
+	console.log("로그인o");
+}*/
+
+// toggle class 'on'
+function toggleOn(){
+	$('.on_js').on('click',function(e){
+		e.preventDefault();
+		$(this).toggleClass('on');
+	});
+}
+
+// tab : '.tab_js' 안에 '.tab_list_js' 와 '.tab_cnt_js'로 구분지어 사용.
+function tab(){
+	$('.tab_js').each(function(){
+		var tabs = $(this).children('.tab_list_js').children('li');
+		var panels = $(this).children('.tab_cnt_js').children('div');
+		var lastTab = tabs.filter('.on');
+		var lastPanel = $(lastTab.children('a').attr('href'));
+		panels.hide();
+		lastPanel.show();
+		tabs.on('click',function(e){
+			e.preventDefault();
+			var thisTab = $(this);
+			var thisPanel = $(thisTab.children('a').attr('href'));
+			lastTab.removeClass('on');
+			thisTab.addClass('on');
+			lastPanel.hide();
+			thisPanel.show();
+			lastTab = thisTab;
+			lastPanel = thisPanel;
+		});
+	})
+}
+
+// tab 모양만
+function tabSwitch(){
+	$('.tab_switch_js').each(function(){
+		var tab = $(this).children('li');
+
+		tab.on('click',function(e){
+			e.preventDefault();
+			tab.removeClass('on');
+			$(this).addClass('on');
+		})
+	})
+}
+
+// 클릭한 영역으로 이동
+function gotoin() {
+	$('.goto_js').each(function(){
+		var gotoTit = $(this).find('a');
+
+		gotoTit.on('click',function(e){
+			e.preventDefault();
+
+			var target = $(this).attr('href');
+			$('.gnb_1depth > li').removeClass('on');
+			$(this).parent().addClass('on');
+
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: $(target).offset().top - 139
+				}, 'slow');
+				
+			}
+		})
+	})
+}
+
+//메뉴 target
+function targetScroll() {
+	var menu = $('.gnb_1depth li');
+	var contents = $('.anchor_section')
+	$(window).scroll(function(){
+		var scltop = $(window).scrollTop();
+		$.each(contents, function(idx){
+			var target = contents.eq(idx);
+			var i = target.index();
+			targetTop = target.offset().top;
+			if (targetTop - 150 <= scltop) {
+				menu.removeClass('on');
+				menu.eq(idx).addClass('on');
+			}
+			if (!(0 <= scltop)) {
+				menu.removeClass('on');
+			}
+		})
+	});	
+};
+
+//swiper 갤러리
+function swiperSlide1() {
+	var swiper1 = new Swiper('.gallery_container', {
+		slidesPerView: 7,
+		spaceBetween: 40,
+		speed: 2000,
+		loop: true,
+		autoplay: {
+			delay: 0,
+			disableOnInteraction: false,
+		},    
+		breakpoints: {
+			1500: {
+				slidesPerView: 6,
+				spaceBetween: 40,
+			},
+			1200: {
+				slidesPerView: 5,
+				spaceBetween: 30,
+			},
+			1024: {
+				slidesPerView: 4,
+				spaceBetween: 10,
+			},
+			768: {
+				slidesPerView: 3,
+				spaceBetween: 10,
+			},
+			480: {
+				slidesPerView: 2,
+				spaceBetween: 10,
+			}
+		}
+	});
+
+	var windowWidth = $(window).outerWidth();
+
+	if(windowWidth > 1025) {
+		$(".gallery_container .swiper-slide").on({
+			'mouseenter focusin' : function(){
+				swiper1.autoplay.stop();
+			},
+			'mouseleave focusout' : function(){
+				swiper1.autoplay.start();
+			}
+		});
+	}
+}
+
+// accordion : '.accordion_js' 안에 '.acd_list_js' 와 '.acd_cnt_js'로 구분지어 사용.
+function accordion(){
+	$('.accordion_js').each(function(){
+		var tabs = $(this).find('.acd_list_js');
+
+		$(this).find('.acd_cnt_js').hide();
+
+		// '.on'이 붙은 아이는 페이지 진입시 열어놓기
+		tabs.filter('.on').next('.acd_cnt_js').show();
+
+		tabs.on('click',function(e){
+			e.preventDefault();
+
+			var thisTab = $(this);
+			var thisPanel = thisTab.next('.acd_cnt_js');
+			var notThisTab = tabs.not(thisTab);
+			var notThisPanel = notThisTab.next();
+
+			if(notThisTab){
+				notThisTab.removeClass('on');
+				notThisPanel.slideUp(300);
+			}
+
+			thisTab.toggleClass('on');
+			thisPanel.stop().slideToggle(300);
+		});
+	})
+}
+
+// selectbox
+function selectBox() {
+	$('.select_form').each(function(){
+		var label = $(this).children('label');
+		var target = $(this).children('.select_custom');
+		var targetName = target.children('option:selected').text();
+
+		label.text(targetName);
+		target.on('change',function(){
+			var targetName = $(this).children('option:selected').text();
+			label.text(targetName);
+		});
+	});
+}
+
+// 테이블 스크롤 커스텀
+function tableScroll() {
+	// 모바일 기기 접속 여부 체크 후 PC일때만 스크롤 mCustomScrollbar 실행
+	var filter = "win16|win32|win64|mac|macintel";
+	if (navigator.platform) {
+		if (filter.indexOf(navigator.platform.toLowerCase()) < 0) {
+			//alert('모바일');
+			//테이블 스크롤  커스터마이징 해제
+			$(".scrollx_tbl_xxl, .scrollx_tbl_xl, .scrollx_tbl_lg, .scrollx_tbl_md, .scrollx_tbl_sm, .scrollx_tbl_xs").mCustomScrollbar("destroy");
+		} else {
+			//alert('PC');
+			//테이블 스크롤  커스터마이징
+			$(".scrollx_tbl_xxl, .scrollx_tbl_xl, .scrollx_tbl_lg, .scrollx_tbl_md, .scrollx_tbl_sm, .scrollx_tbl_xs").mCustomScrollbar({
+				axis: "x",
+				theme: "dark",
+				advanced: {
+					autoExpandHorizontalScroll: true,
+					updateOnContentResize: true
+				}
+			});
+		}
+	}
+}
+
+// 스크롤 패럴럭스
+function setScrollEffect(selector, extra) {
+	checkVisibility();
+	$(window).on('scroll resize', function() {
+		checkVisibility();
+	});
+
+	function checkVisibility() {
+		$(selector).each(function(){
+			var target = $(this);
+			var scrollTop = $(document).scrollTop();
+			var minShow = target.offset().top - $(window).height() * extra;
+
+			if ( scrollTop >= minShow ){
+				target.addClass('on');
+			}
+		});
+	}
+}
+
+// 브라우저 알림창
+function browserAlert(){
+	$(".browseralert_close").on("click", function() {
+		$("#browseralert").slideUp();
+	});
+}
+
+// IE 버전 체크 (UserAgent)
+var ua = navigator.userAgent.toLowerCase();
+// IE7엔 브라우저 엔진명인 Trident가 없고 IE11엔 MSIE란 문자열이 없으므로 두 가지 경우를 모두 체크.
+if( ua.indexOf( 'msie' ) != -1 || ua.indexOf( 'trident' ) != -1 ) {
+	var version = 11;
+	ua = /msie ([0-9]{1,}[\.0-9]{0,})/.exec( ua );
+	if( ua )
+	{
+		version = parseInt( ua[ 1 ] );
+	}
+	var classNames = '';
+	// 기존 방식에 is-ie 라는 클래스 추가
+	classNames += ' is-ie';
+	// 기존 방식에 현재 버전 클래스 추가
+	classNames += ' ie' + version;
+	for( var i = version + 1; i <= 11; i++ ) {
+		classNames +=  ' lt-ie' + i;
+	}
+	// html 태그에 클래스 추가
+	document.getElementsByTagName( 'html' )[ 0 ].className += classNames;
+}
+
+$(document).ready(function () {
+
+	// toggle class 'on'
+	toggleOn();
+
+	// tab 기본
+	tab();
+
+	// tab 모양만
+	tabSwitch();
+
+	// 클릭한 영역으로 이동
+	gotoin();
+
+	//메뉴 target
+	targetScroll();
+
+	//swiper 갤러리
+	swiperSlide1();
+
+	// accordion
+	accordion();
+
+	// selectbox
+	selectBox();
+
+	// 스크롤 패럴럭스
+	setScrollEffect('.fadeup', 0.9);
+	setScrollEffect('.fadein', 0.9);
+
+	// 브라우저 알림창
+	browserAlert();
+
+
+	// select2 설정
+	$(".sel_search_row select").select2({
+		formatNoMatches: function() {
+			return '결과가 없습니다.';
+		},
+	});
+	$(".sel_search_row.all select").select2({
+		placeholder: "전체",
+	});
+
+	// 이미지 라이트박스
+	$('.openimg').magnificPopup({
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		callbacks: {
+			resize: changeImgSize,
+			imageLoadComplete: changeImgSize,
+			change: changeImgSize
+		}
+	});
+
+	function changeImgSize() {
+		var img = this.content.find('img');
+		img.css('max-height', '100%');
+		img.css('height', 'auto');
+		img.css('width', 'auto');
+		img.css('max-width', '810px');
+	}
+
+});
+
+$(window).on("load", function () {
+	tableScroll();
+});
+
+// outline 설정 - 키보드로 접근시엔 아웃라인을 보여주고 마우스로 접근할때는 아웃라인을 없애줌
+(function (d) {
+	var style_element = d.createElement('STYLE'),
+		dom_events = 'addEventListener' in d,
+		add_event_listener = function (type, callback) {
+			// Basic cross-browser event handling
+			if (dom_events) {
+				d.addEventListener(type, callback);
+			} else {
+				d.attachEvent('on' + type, callback);
+			}
+		},
+		set_css = function (css_text) {
+			// Handle setting of <style> element contents in IE8
+			!!style_element.styleSheet ? style_element.styleSheet.cssText = css_text : style_element.innerHTML = css_text;
+		};
+
+	d.getElementsByTagName('HEAD')[0].appendChild(style_element);
+
+	// Using mousedown instead of mouseover, so that previously focused elements don't lose focus ring on mouse move
+	/*add_event_listener('mousedown', function () {
+		set_css(':focus{outline:0}::-moz-focus-inner{border:0;}');
+	});*/
+	add_event_listener('keydown', function () {
+		set_css(':focus{outline:dotted 1px #193296}::-moz-focus-inner{border:dotted 1px #193296;}');
+	});
+})(document);
+
+
